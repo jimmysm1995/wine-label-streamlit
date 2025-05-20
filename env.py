38cfcb4ef1fd10d45f8c4e1b04995cd5f93845fd
josesm <jose.s.m.1995@gmail.com>
2025-05-20 21:34:31 +0200

    update mongo

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 BLOB_STORAGE_STRING = 'DefaultEndpointsProtocol=https;AccountName=vinokblob1;AccountKey=ldNEZ6QMc9VruS1FxnlwDqDZwTeoAPQNH0OZRySejaK3U1Q6lQponqvFyobtOPnrtbG5BbUOSJYl+AStbWQR3g==;EndpointSuffix=core.windows.net'
-MONGO_CONECTION_STRING = 'mongodb://root:root@localhost:27018/'
+MONGO_CONECTION_STRING = 'mongodb://vinok-pre:4AhBFNOEFxx2b8nQolO61N5yTRqI2WN5gu2Yfxs5VzuK9eCe7ZnY7TcQT6MDi8CLrMzgEB7njEl1ACDbKml9Sw%3D%3D@vinok-pre.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@vinok-pre@'
 AZURE_EMBEDDING_URL='https://jose-m8sjluwq-eastus2.cognitiveservices.azure.com/openai/deployments/text-embedding-3-small'
 AZURE_EMBEDDING_KEY='74GJC2c7loF6VpqhCUqWEUJ82Eg9ZS163XZXFmcAJ9bbp7bSCGalJQQJ99BCACHYHv6XJ3w3AAAAACOGrSgN'
 AZURE_EMBEDDING_VERSION='2023-05-15'
